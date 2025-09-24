d46772b26e1e63f99c41bf2c9cea6036e841246c
olusegun <olusegunbamgbelu755@gmail.com>
2025-09-24 15:52:28 +0100

    feat:sidebar-layout
    worked on the sidebar display on mobile and desktop

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,5 @@
+export default function Sidebar() {
+  return (
+    <div className='bg-[url("./bg-sidebar-mobile.svg")] h-42.5 w-full md:w-[270px] bg-cover bg-center bg-no-repeat md:h-full md:rounded-[5px] md:bg-[url("./bg-sidebar-desktop.svg")]'></div>
+  );
+}
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,11 @@
+import Sidebar from '../components/sidebar/Sidebar';
+
 export default function RootLayout() {
-  return <div className="w-full h-screen">
-    
-  </div>
+  return (
+    <div className='w-full h-screen bg-body-background md:flex md:justify-center md:items-center'>
+      <div className='md:w-[950px] bg-white md:h-[600px] md:p-4 md:rounded-[5px]'>
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
