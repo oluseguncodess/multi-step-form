8ac9d1c316e79c16b8931d35659539f9b0037a2f
olusegun <olusegunbamgbelu755@gmail.com>
2025-10-07 13:43:59 +0100

    fix:usenavigate hook logic

--- src/components/confirmation/ConfirmationPage.tsx
+++ src/components/confirmation/ConfirmationPage.tsx
@@ -1,18 +1,21 @@
 import { useEffect } from 'react';
 import thankYou from '../../assets/images/icon-thank-you.svg';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function ConfirmationPage() {
+  const navigate = useNavigate();
   useEffect(() => {
-    <Navigate to='/' replace/>
-  }, [])
-  
+    navigate('/', { replace: true });
+  }, [navigate]);
+
   return (
     <div className='flex flex-col items-center gap-4'>
       <div className='w-15 h-15 mb-3 md:w-22 md:h-22'>
         <img src={thankYou} alt='thank-you' className='w-full h-full' />
       </div>
-      <h1 className='text-primary-blue-950 text-2xl font-bold md:text-[2.05rem] md:tracking-wide'>Thank you!</h1>
+      <h1 className='text-primary-blue-950 text-2xl font-bold md:text-[2.05rem] md:tracking-wide'>
+        Thank you!
+      </h1>
       <p className='text-center text-gray-400 w-full max-w-[300px] md:max-w-full'>
         Thanks for confirming your subscription! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
